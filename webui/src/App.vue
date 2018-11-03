975fd58ec8e0461485b13f0938b06b16994e4bbb
moznion <moznion@gmail.com>
2018-11-03 13:11:51 +0900

    Make a scaffold for web UI

---
+++ webui/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <h1>SECELF</h1>
+    <button v-on:click="foo">Reverse Message</button>
+{{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    foo: function() {
+      this.msg = 'hih';
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  margin-top: 10px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
