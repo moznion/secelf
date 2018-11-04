76213c452c62c91c22e365d455bc07aad242e383
moznion <moznion@gmail.com>
2018-11-04 23:10:49 +0900

    Fix

--- webui/src/App.vue
+++ webui/src/App.vue
@@ -5,7 +5,7 @@
     <form action="/api/files" method="POST" enctype="multipart/form-data">
       <div class="input-group">
         <div class="custom-file">
-          <input type="file" class="custom-file-input" id="file">
+          <input type="file" class="custom-file-input" id="file" name="file">
           <label class="custom-file-label" for="file">Choose file</label>
         </div>
         <div class="input-group-append">
